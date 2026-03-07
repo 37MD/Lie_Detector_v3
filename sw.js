b09686d5dd8e1b2277b658341023105dc1383c32
37MD <150575622+37MD@users.noreply.github.com>
2026-03-07 21:17:34 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lie-detector-cache-v2';
+const CACHE_NAME = 'lie-detector-cache-v2.05';
 const urlsToCache = [
   './',
   './index.html',
@@ -7,11 +7,29 @@ const urlsToCache = [
 
 // Install and Cache
 self.addEventListener('install', event => {
+  // force the waiting service worker to become the active service worker
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
@@ -26,4 +44,4 @@ self.addEventListener('fetch', event => {
         return fetch(event.request); // Otherwise fetch from network
       })
   );
-});
+});s
